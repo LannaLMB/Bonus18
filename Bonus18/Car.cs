f6df33b76bf7b5b67e75c4dc3086499c00b7dd4a
Lanna <Lanna_lmb@hotmail.com>
2017-04-21 15:15:48 -0400

    Initial Commit

---
+++ Bonus18/Car.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Car
+    {
+
+        #region Variables
+
+        private string Make;
+        private string Model;
+        private int Year;
+        private double Price;
+
+        #endregion
+
+
+
+        #region Properties
+
+        // Property - Make
+        public string CarMake
+        {
+            set
+            {
+                Make = value;
+            }
+
+            get
+            {
+                return Make;
+            }
+        }
+
+
+
+        // Property - Model
+        public string CarModel
+        {
+            get
+            {
+                return Model;
+            }
+
+            set
+            {
+                Model = value;
+            }
+        }
+
+
+
+        // Property - Year
+        public int CarYear
+        {
+            get
+            {
+                return Year;
+            }
+
+            set
+            {
+                Year = value;
+            }
+        }
+
+
+
+        // Property - Price
+        public double CarPrice
+        {
+            get
+            {
+                return Price;
+            }
+
+            set
+            {
+                Price = value;
+            }
+        }
+
+        #endregion
+
+
+
+
+
+        public Car (string MakeInput,
+            string ModelInput,
+            int YearInput,
+            double PriceInput)
+        {
+
+            Make = MakeInput;
+            Model = ModelInput;
+            Year = YearInput;
+            Price = PriceInput;
+
+        }
+
+        public Car()
+        {
+            Make = "Jeep";
+            Model = "Liberty";
+            Year = 2006;
+            Price = 20000.02;
+
+        }
+    }
+}
---
+++ Bonus18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Welcome Message
+            Console.WriteLine("Welcome to the Grand Circus Motors Admin Console!");
+            Console.WriteLine();
+
+
+            // Car1 Data
+            Car Car1 = new Car();
+
+
+            //Make Data
+            Console.Write("Please Enter the Make of Your Car:   --->   ");
+            Car1.CarMake = Console.ReadLine();
+            Console.WriteLine();
+
+
+            // Model Data
+            Console.Write("Please Enter the Model of Your Car:   --->   ");
+            Car1.CarModel = Console.ReadLine();
+            Console.WriteLine();
+
+
+            // Year Data
+            Console.Write("Please Enter the Year of Your Car:   --->   ");
+            Car1.CarYear = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+            // Price Data
+            Console.Write("Please Enter the Price of Your Car:   --->   ");
+            Car1.CarPrice = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            Car Car2 = new Car();
+            Console.WriteLine(Car1.CarMake +" "+ Car1.CarModel +" "+ Car1.CarYear +" "+ Car1.CarPrice);
+
+
+        }
+    }
+}
